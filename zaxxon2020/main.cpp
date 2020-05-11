3c901de9e9f580a920370998c5e38d8bb133df45
Allan <a.maubert.jouvet@gmail.com>
2020-05-11 19:15:51 +0200

    add precompiled header

--- zaxxon2020/main.cpp
+++ zaxxon2020/main.cpp
@@ -1,10 +1,10 @@
-#include <SFML/Graphics.hpp>
+#include "pch.h"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
---
+++ zaxxon2020/pch.h
@@ -0,0 +1,32 @@
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: add headers that you want to pre-compile here
+#include <stdio.h>
+#include <cstdlib>
+#include <tchar.h>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+#pragma comment(lib, "sfml-audio-d.lib")
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-main-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "opengl32.lib")
+
+#endif //PCH_H
