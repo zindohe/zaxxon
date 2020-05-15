232805c449f177927f4b8fbdee37c18e8121165c
Zineddine <zinedddine97@gmail.com>
2020-05-15 17:39:10 +0200

    Add keyboard key handling // TO CLEAN

--- zaxxon2020/Game.cpp
+++ zaxxon2020/Game.cpp
@@ -1,27 +1,95 @@
 #include "pch.h"
 #include "Game.h"
 
+const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
+
 Game::Game() : mainWindow(sf::VideoMode(840, 600), "SFML works!", sf::Style::Close)
              , shape(100.f){
-    shape.setFillColor(sf::Color::Red);
+    shape.setFillColor(sf::Color::Green);
+    if (!testFont.loadFromFile("Media/Sansation.ttf"))
+    {
+        printf("ERREUR : police non chargee");
+    }
+
+    init();
 }
 Game::~Game() {
 
 }
 
+void Game::init()
+{
+    testText.setString("NO MOVE");
+    testText.setFont(testFont);
+    testText.setPosition(0.f, 0.f);
+    testText.setCharacterSize(24);
+    testText.setFillColor(sf::Color::Blue);
+}
+
 void Game::run() {
 
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
     while (mainWindow.isOpen())
     {
-        sf::Event event;
-        while (mainWindow.pollEvent(event))
+        sf::Time elapsedTime = clock.restart();
+        timeSinceLastUpdate += elapsedTime;
+        while (timeSinceLastUpdate > TimePerFrame)
         {
-            if (event.type == sf::Event::Closed)
-                mainWindow.close();
+            timeSinceLastUpdate -= TimePerFrame;
+
+            processEvents();
+            update(TimePerFrame);
         }
 
-        mainWindow.clear();
-        mainWindow.draw(shape);
-        mainWindow.display();
+        render();
     }
 }
+
+void Game::processEvents() {
+
+    sf::Event event;
+    while (mainWindow.pollEvent(event)) {
+        switch (event.type)
+        {
+        case sf::Event::KeyPressed:
+            handlePlayerActions(event.key.code, true);
+            break;
+        case sf::Event::KeyReleased:
+            handlePlayerActions(event.key.code, false);
+            break;
+        case sf::Event::Closed:
+            mainWindow.close();
+            break;
+        }
+    }
+}
+
+void Game::render() 
+{
+    mainWindow.clear();
+    mainWindow.draw(testText);
+    mainWindow.display();
+}
+
+void Game::handlePlayerActions(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::Up)
+        isUpPressed = isPressed;
+    else if (key == sf::Keyboard::Down)
+        isDownPressed = isPressed;
+    else if (key == sf::Keyboard::Left)
+        isLeftPressed = isPressed;
+    else if (key == sf::Keyboard::Right)
+        isRightPressed = isPressed;
+}
+
+void Game::update(sf::Time elapsedTime)
+{
+    if (isUpPressed) {
+        testText.setString("UP");
+    } else if (isDownPressed) {
+        testText.setString("DOWN");
+    }
+}
+
--- zaxxon2020/Game.h
+++ zaxxon2020/Game.h
@@ -7,7 +7,21 @@ class Game
 		void run();
 	
 	private:
+		void processEvents();
+		void handlePlayerActions(sf::Keyboard::Key key, bool isPressed);
+		void update(sf::Time elapsedTime);
+		void render();
+		void init();
+
 		sf::RenderWindow mainWindow; // Fenêtre Principale où l'on va poser des elements graphique
 		sf::CircleShape shape;
+		static const sf::Time	TimePerFrame;
+		bool isUpPressed = false;
+		bool isDownPressed = false;
+		bool isLeftPressed = false;
+		bool isRightPressed = false;
+
+		sf::Text testText;
+		sf::Font testFont;
 };
 
